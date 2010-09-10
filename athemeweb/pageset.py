8eb08b4b07dd9842cb5d31d56fbe78a540718301
William Pitcock <nenolod@atheme.org>
2010-09-09 19:55:30 -0500

    Add pageset code.

---
+++ athemeweb/pageset.py
@@ -0,0 +1,51 @@
+#
+# Copyright (c) 2010 William Pitcock <nenolod@atheme.org>.
+#
+# This file is licensed under the Atheme license.
+#
+
+def build_page_set(conn):
+    pageset = {
+        'account': {
+            'title': "My Account",
+            'privs': None
+        },
+        'channel/list': {
+            'title': "My Channels",
+            'privs': None
+        },
+        'memo/list': {
+            'title': "Memo Inbox",
+            'privs': None
+        },
+        'memo/write': {
+            'title': "New Memo",
+            'privs': None
+        },
+        'akill/list': {
+            'title': "AKILLs",
+            'privs': 'operserv:akill',
+        },
+        'logout': {
+            'title': "Logout",
+            'privs': None
+        },
+    }
+
+    realset = {}
+    for i in pageset.keys():
+        page = pageset[i]
+        if page['privs'] is None or conn.has_privilege(page['privs']):
+            realset[i] = page
+
+    return realset
+
+if __name__ == '__main__':
+    from config import XMLRPC_PATH
+    from athemeconnection import AthemeXMLConnection
+
+    x = AthemeXMLConnection(XMLRPC_PATH)
+    x.login('someuser', 'somepass')
+
+    from pprint import pprint
+    pprint(build_page_set(x))
