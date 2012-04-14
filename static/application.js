258dad0289d4494e1da5b9549598186d0ee2ffa1
William Pitcock <nenolod@dereferenced.org>
2012-04-14 09:47:47 -0500

    appshell: rewrite links to delegate to ajax loader function
    (this way non-modal servlets don't exit the appshell...)

--- static/application.js
+++ static/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
 	$("input:submit").button();
-	$("#tabs").tabs();
+	$("#tabs").tabs({
+		load: function(event, ui) {
+			$(ui.panel).delegate('a', 'click', function(event) {
+				$(ui.panel).load(this.href);
+				event.preventDefault();
+			});
+		}
+	});
 });
